f1cc75a366abbe1830e8fc200010e5283451d799
Yana-Park <yanna73kr@gmail.com>
2023-03-15 23:40:39 +0000

    Add function to jsquiz.js file

---
+++ jsquiz.js
@@ -0,0 +1,22 @@
+function getResults() {
+    var amountCorrect = 0;
+    for(var i = 0; i <= 3; i++) {
+        var radioName = document.getElementsByName('answer' + i);
+    for (var j = 0; j < radioName.length; j++) {
+        var radiosValue = radioName[j];
+        if (radiosValue.checked) {
+            if (radiosValue.value == "correct") {
+                amountCorrect++;
+                radiosValue.nextSibling.style.color = "green";
+            } else {
+                radiosValue.nextSibling.style.color = "red";
+            } 
+            } else {
+            radiosValue.nextSibling.style.color = "";
+            }
+        }
+     }
+     document.getElementById('results').innerHTML = 
+     "Correct answers: " + amountCorrect;
+    } //https://teamtreehouse.com/community/javascript-project-simple-quiz-with-radio-buttons
+
