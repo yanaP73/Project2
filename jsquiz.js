d75567bf1185d0b753aeccd4a4fbc3cbfc0a22a9
Yana-Park <yanna73kr@gmail.com>
2023-03-20 19:21:05 +0000

    Change variable type var to let

--- jsquiz.js
+++ jsquiz.js
@@ -1,27 +1,28 @@
-function playAudio(){
+"use strict"
+function playAudio() {
     document.getElementById("mp3_src").src = "bckgmusic.mp3";
-    document.getElementById("audio").onload();
+    
 }
 function getResults() {
     var amountCorrect = 0;
-    for(var i = 0; i <= 3; i++) {
-        var radioName = document.getElementsByName('answer' + i);
-    for (var j = 0; j < radioName.length; j++) {
-        var radiosValue = radioName[j];
-        if (radiosValue.checked) {
-            if (radiosValue.value == "correct") {
-                amountCorrect++;
-                radiosValue.nextSibling.style.color = "green";
-            } else {
-                radiosValue.nextSibling.style.color = "red";
-            } 
-            } else {
-            radiosValue.nextSibling.style.color = "";
+    for (let i = 1; i <= 3; i++) {
+        let radioName = document.getElementsByName('answer' + i);
+        for (let j = 0; j < radioName.length; j++) {
+            let radiosValue = radioName[j];
+                if (radiosValue.checked) {
+                if (radiosValue.value == "correct") {
+                    amountCorrect++;
+                    radiosValue.nextSibling.style.color = "green";
+                } else {
+                    radiosValue.nextSibling.style.color = "red";
+                } 
+                } else {
+                radiosValue.nextSibling.style.color = "";
+                }
             }
         }
-     }
-     document.getElementById('results').innerHTML = 
-     "Correct answers: " + amountCorrect;
+        document.getElementById('results').innerHTML = 
+        "Correct answers: " + amountCorrect;
     } //https://teamtreehouse.com/community/javascript-project-simple-quiz-with-radio-buttons
     
     
